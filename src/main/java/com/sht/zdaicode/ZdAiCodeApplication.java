33725340f0d8529140c296e90706ff62d7dcc33a
1150550070 <1150550070@qq.com>
2025-09-02 12:03:20 +0800

    初始化项目 添加依赖和配置(HuToll 和 Knife4j接口文档) 自定义异常 请求响应包装类 基础接口

---
+++ src/main/java/com/sht/zdaicode/ZdAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.sht.zdaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZdAiCodeApplication.class, args);
+    }
+
+}
