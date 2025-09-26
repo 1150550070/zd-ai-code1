4163ae2ec1a2efbeb9926d485c8b7023faa8aace
1150550070 <1150550070@qq.com>
2025-09-26 21:07:15 +0800

    可观测性 ARMS系统监控

---
+++ src/main/java/com/sht/zdaicode/monitor/MonitorContext.java
@@ -0,0 +1,26 @@
+package com.sht.zdaicode.monitor;
+
+import com.sht.zdaicode.model.enums.CodeGenTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MonitorContext implements Serializable {
+
+    private String userId;
+
+    private String appId;
+    
+    private CodeGenTypeEnum codeGenType;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
